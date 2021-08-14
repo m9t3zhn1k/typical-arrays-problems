df41a9d96facef386ca5f7d338971491392b0460
m9t3zhn1k <m9t3zhn1k@mail.ru>
2021-08-14 11:40:07 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minItem = 0;
+  if (!array) return 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minItem) minItem = array[i];
+  }
+  return minItem;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxItem = 0;
+  if (!array) return 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > maxItem) maxItem = array[i];
+  }
+  return maxItem;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  let result = 0;
+  if (!array || array.length < 1) return 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  result = sum / array.length;
+  return result;
 }
